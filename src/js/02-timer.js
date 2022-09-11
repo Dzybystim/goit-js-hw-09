130ee1bd4892373111a7d98468795a71ca0fe2f1
Dzybystim <103567184+Dzybystim@users.noreply.github.com>
2022-09-11 20:04:17 +0300

    finnaly

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,5 @@
 import flatpickr from "flatpickr";
+import Notiflix from 'notiflix';
 import "flatpickr/dist/flatpickr.min.css";
 
 const datetimePickerElement = document.querySelector('input#datetime-picker');
@@ -7,8 +8,45 @@ const daysElement = document.querySelector('span[data-days]');
 const hoursElement = document.querySelector('span[data-hours]');
 const minutesElement = document.querySelector('span[data-minutes]');
 const secondsElement = document.querySelector('span[data-seconds]');
+const timerElement = document.querySelector('.timer');
+const fieldElement = document.querySelectorAll('.field');
+const labelElement = document.querySelectorAll('.label');
+const valueElement = document.querySelectorAll('.value');
 
+// Добавляю стили
 buttonStartElement.disabled = "true";
+buttonStartElement.style.display = "inline-flex";
+buttonStartElement.style.margin = "5px";
+buttonStartElement.style.border = "2px solid #4C4B63";
+buttonStartElement.style.fontSize = "10px";
+buttonStartElement.style.lineHeight = "10px";
+buttonStartElement.style.padding = "6px 15px";
+buttonStartElement.style.color = "#FFF";
+buttonStartElement.style.fontWeight = "bold";
+buttonStartElement.style.textTransform = "uppercase";
+buttonStartElement.style.background = "#ABA8B2";
+
+timerElement.style.display = "flex";
+
+fieldElement.forEach(function callback(element){
+  element.style.padding = "10px";
+});
+
+labelElement.forEach(function callback(element){
+  element.style.display = "block";
+  element.style.textAlign = "center";
+  element.style.fontSize = "12px"
+})
+
+valueElement.forEach(function callback(element){
+  element.style.display = "block"
+  element.style.textAlign = "center";
+  element.style.fontWeight = "bold";
+  
+})
+
+//Добавлять стили закончил
+
 
 const options = {
     enableTime: true,
@@ -17,41 +55,38 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
       console.log(selectedDates[0]);
-      buttonStartElement.removeAttribute('disabled');
 
       const date = new Date();
       const nowDateMs = date.getTime();
       const changeDateMs = selectedDates[0].getTime()
      
-      if(nowDateMs > changeDateMs) {
-        return window.alert("Please choose a date in the future");
-      } else {
-        
-    function timeRecord(event) {
+    if(nowDateMs > changeDateMs) {
+        return Notiflix.Report.info("Please choose a date in the future");
+      } 
+    buttonStartElement.removeAttribute('disabled');
 
-    let diff = changeDateMs-nowDateMs;
-    let diffConvertMs = convertMs(diff);
-
-    daysElement.textContent = diffConvertMs.days
-    hoursElement.textContent = diffConvertMs.hours
-    minutesElement.textContent = diffConvertMs.minutes
-    secondsElement.textContent = diffConvertMs.seconds
-
-       }
+    buttonStartElement.addEventListener('click', () => {
+      setInterval(() => {
+        let diff = changeDateMs-new Date();
+        let diffConvertMs = convertMs(diff);
 
 
+        daysElement.textContent = diffConvertMs.days<=0 ? '00': addLeadingZero(diffConvertMs.days);
+        hoursElement.textContent = diffConvertMs.hours<=0 ? '00':  addLeadingZero(diffConvertMs.hours);
+        minutesElement.textContent = diffConvertMs.minutes<=0 ? '00':  addLeadingZero(diffConvertMs.minutes);
+        secondsElement.textContent = diffConvertMs.seconds<=0 ? '00':  addLeadingZero(diffConvertMs.seconds);
 
-    buttonStartElement.addEventListener('click', timeRecord);
 
-    
-
-      }
-
+       function addLeadingZero(value){
+          if(value < 10) {
+            return "0"+value
+          }
+          return value
+        }
       
-
+      }, 1000)})
 
     },
-
 
   };
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
+import Notiflix from 'notiflix';
+
+
+const formElement = document.querySelector('.form');
+
+function create(event) {
+  event.preventDefault();
+
+  const {elements: { delay, step, amount}
+} = event.currentTarget;
+
+let delayValue = Number(delay.value);
+let stepValue = Number(step.value);
+let amountValue = Number(amount.value);
+
+for(let i=1; i<=amountValue; i+=1){
+  createPromise(i, delayValue)
+  .then(({position, delay}) => {
+    setTimeout(()=>{
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    }, delay);
+  })
+.catch(({position, delay}) => {
+  setTimeout(()=>{
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+  }, delay);
+})
+delayValue+=stepValue
+}
+
+}
+  
+
+formElement.addEventListener("submit", create)
+
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) =>
+   {    setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve({position, delay});
   } else {
-    // Reject
+    reject({position, delay})
   }
+   }, delay)
+  }
+  )
 }
+
+
+
